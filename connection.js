9c1217791623201f4ce60ecf8a4e7eca9952db8e
cacolukia <cacolukia@naver.com>
2018-06-11 21:45:28 +0900

    TEST

---
+++ connection.js
@@ -0,0 +1,13 @@
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017/example';
+
+MongoClient.connect(url, (err, database) => {
+   if (err) {
+      console.log('MongoDB 연결 실패');
+      return;
+   }
+   console.log("MongoDB 연결 성공");
+
+   // 연결 종료하기
+   database.close();
+});
